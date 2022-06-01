3c82621ebf81ac1e9ad707ee00219e817508e359
Joshua Diaz <jad24096@gmail.com>
2022-05-31 21:58:29 -0400

    pseudocoded out the app page with requirements from the project.

--- reactportfolio_jd/src/App.js
+++ reactportfolio_jd/src/App.js
@@ -1,10 +1,52 @@
 import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
 
 function App() {
+// {/* the corresponding nav-tile to the current page is highlighted */}
+// About me page is default on first load
+
+function aboutMe() {
+// working nav bar wiht corresponding pages 
+// the about me section has a profile picture and short bio
+}
+
+function portfolio() {
+//wokring portfolio page with 6 projects with live/repo links
+}
+
+function projects() {
+//needs an img
+//title
+//live link
+//repo link
+}
+
+function contact() {
+//contact form which takes in name, email, and a message
+//notification that certain fields on the form are required in the cursor leaves area
+//error checking for invalid email
+}
+
+function resume() {
+//link to a downloadable resume 
+//list of skills
+}
+
+function footer() {
+//links to github, linked in, etc. 
+}
+
+
   return (
+    <>
     <header>
-      My Github: {Github}, My LinkedIn: {LinkedIn} My Projects: {Projects}
+    {/* need name */}
+    {/* need the following tiles -> About Me, Portfolio, Contact, Resume */}
+    
     </header>
+    <div class="projImg">{project}</div>
+    <Footer>This is the Footer</Footer>
+    </>
   );
 }
 
