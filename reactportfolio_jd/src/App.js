103f0f0955478562f633117c1b3a0ae6cef94097
Joshua Diaz <jad24096@gmail.com>
2022-05-31 23:07:26 -0400

    last commit for the night.

--- reactportfolio_jd/src/App.js
+++ reactportfolio_jd/src/App.js
@@ -3,84 +3,86 @@ import React, { useState, useRef, useEffect } from 'react'
 import styled from 'styled-components';
 
 function App() {
- 
-const Button = styled.button`
+  function nav() {
+    const Button = styled.button`
   /* Same as above */
 `;
-const ButtonToggle = styled(Button)`
+    const ButtonToggle = styled(Button)`
   opacity: 0.6;
   ${({ active }) =>
-    active &&
-    `
+        active &&
+        `
     opacity: 1;
   `}
 `;
-const ButtonGroup = styled.div`
+    const ButtonGroup = styled.div`
   display: flex;
 `;
-const types = ['About Me', 'Portfolio', 'Contact', 'Resume'];
-function ToggleGroup() {
-  const [active, setActive] = useState(types[0]);
-  return (
-    <ButtonGroup>
-      {types.map(type => (
-        <ButtonToggle
-          key={type}
-          active={active === type}
-          onClick={() => setActive(type)}
-        >
-          {type}
-        </ButtonToggle>
-      ))}
-    </ButtonGroup>
-  );
-
-// {/* the corresponding nav-tile to the current page is highlighted */}
-// About me page is default on first load
+    const types = ['About Me', 'Portfolio', 'Contact', 'Resume'];
+    function ToggleGroup() {
+      const [active, setActive] = useState(types[0]);
+      return (
+        <ButtonGroup>
+          {types.map(type => (
+            <ButtonToggle
+              key={type}
+              active={active === type}
+              onClick={() => setActive(type)}
+            >
+              {type}
+            </ButtonToggle>
+          ))}
+        </ButtonGroup>
+      );
+    }
   }
 
+  // {/* the corresponding nav-tile to the current page is highlighted */}
+  // About me page is default on first load
 
-function aboutMe() {
-// working nav bar wiht corresponding pages 
-// the about me section has a profile picture and short bio
-}
 
-function portfolio() {
-//wokring portfolio page with 6 projects with live/repo links
-}
 
-function projects() {
-//needs an img
-//title
-//live link
-//repo link
-}
+  function aboutMe() {
+    // working nav bar wiht corresponding pages 
+    // the about me section has a profile picture and short bio
+  }
 
-function contact() {
-//contact form which takes in name, email, and a message
-//notification that certain fields on the form are required in the cursor leaves area
-//error checking for invalid email
-}
+  function portfolio() {
+    //wokring portfolio page with 6 projects with live/repo links
+  }
 
-function resume() {
-//link to a downloadable resume 
-//list of skills
-}
+  function projects() {
+    //needs an img
+    //title
+    //live link
+    //repo link
+  }
 
-function footer() {
-//links to github, linked in, etc. 
-}
+  function contact() {
+    //contact form which takes in name, email, and a message
+    //notification that certain fields on the form are required in the cursor leaves area
+    //error checking for invalid email
+  }
+
+  function resume() {
+    //link to a downloadable resume 
+    //list of skills
+  }
+
+  function footer() {
+    //links to github, linked in, etc. 
+  }
 
 
   return (
     <>
-    <header>
-    {/* need name */}
-    {/* need the following tiles -> About Me, Portfolio, Contact, Resume */}
-    <button onClick="highlight"></button>
-    </header>
-    <div class="projImg">{project}</div>
-    <Footer>This is the Footer</Footer>
+      <header>
+        {/* need name */}
+        {/* need the following tiles -> About Me, Portfolio, Contact, Resume */}
+        <button onClick="highlight"></button>
+      </header>
+      <div class="projImg">{project}</div>
+      <Footer>This is the Footer</Footer>
     </>
   );
 }
