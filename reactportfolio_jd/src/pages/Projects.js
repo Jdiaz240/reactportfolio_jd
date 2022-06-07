559614c8b2fb179713d331764cbb0e8f18d4e6bc
Joshua Diaz <jad24096@gmail.com>
2022-06-07 10:16:29 -0400

    updated models.

---
+++ reactportfolio_jd/src/pages/Projects.js
@@ -0,0 +1,44 @@
+
+
+
+function Projects() {
+
+    return (
+        <>
+            <section>
+                <div id="project1">
+                    <Image
+                        src=""
+                    />
+                    <button>Code quiz</button>
+                </div>
+                <div id="project2">
+                    <Image
+                        src=""
+                    />
+                    <button>Day Planner</button>
+                </div>
+                <div id="project3">
+                    <Image
+                        src=""
+                    />
+                    <button>Note Taker</button>
+                </div>
+                <div id="project4">
+                    <Image
+                        src=""
+                    />
+                    <button>Social Network Api</button>
+                </div>
+                <div id="project5">
+                    <Image
+                        src=""
+                    />
+                    <button>Regex Tutorial</button>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Projects;
